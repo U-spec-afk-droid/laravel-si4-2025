fc61b4e8c641696c3a2aa6ad6e4102cbf7d8c4dc
ummi aulia <auliaummi807@gmail.com>
2025-10-18 13:00:42 +0700

    Belajara mengenai table

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -36,8 +36,43 @@
   </div>
 </nav>
 
- <div class="container" style="margin-left:30px">    
+ <div class="container" >    
   <h1>Ini adalah halaman Mahasiswa</h1>
+
+ <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+  <thead>
+    <tr>
+      <th>NIM</th>
+      <th>Nama Mahasiswa</th>
+      <th>Jenis Kelamin</th>
+      <th colspan="2">TTL</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>123</td>
+      <td>Ummi Aulia</td>
+      <td>Perempuan</td>
+      <td>08-03-2005</td>
+      <td rowspan="3">Medan</td>
+    </tr>
+    <tr>
+      <td>124</td>
+      <td>Miftah</td>
+      <td>Perempuan</td>
+      <td>10-05-2004</td>
+  
+    </tr>
+    <tr>
+      <td>125</td>
+      <td>Indah</td>
+      <td>Perempuan</td>
+      <td>15-01-2003</td>
+    
+    </tr>
+  </tbody>
+</table>
+
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
